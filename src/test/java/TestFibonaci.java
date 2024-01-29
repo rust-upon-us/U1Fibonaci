7fedafa31a2e442806f92a9e76cf7988a337bfde
Christian <chrstian.cardo.dev@gmail.com>
2024-01-29 15:41:03 +0100

    First implementation

--- src/test/java/TestFibonaci.java
+++ src/test/java/TestFibonaci.java
@@ -1,11 +1,11 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class TestFibonaci {
+    private App main = new App();
     @Test
     @DisplayName("Fibonaci Test 0")
     void testFibbonaci(){
@@ -16,14 +16,14 @@ public class TestFibonaci {
     @DisplayName("Fibonaci Test 1")
     void testFibbonaci1(){
         int[] expected = {0};
-        int[] returned = App.fibonaciCalculator(1);
+        int[] returned = main.fibonaciCalculator(1);
         assertArrayEquals(expected,returned);
     }
     @Test
     @DisplayName("Fibonaci Test 5")
     void testFibbonaci5(){
         int[] expected = {0,1,1,2,3};
-        int[] returned = App.fibonaciCalculator(5);
+        int[] returned = main.fibonaciCalculator(5);
         assertArrayEquals(expected,returned);
     }
 
